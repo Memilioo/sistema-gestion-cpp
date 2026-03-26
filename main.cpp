2a2390722d0730c6525856386dfa82771eece743
emilioreyess <ereyesa10@miumg.edu.gt>
2026-03-25 22:56:46 -0600

    Se implementa menu interactivo con switch y do-while

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
-    
+#include <iostream>
+using namespace std;
+
+int main() {
+    int opcion;
+
+    do {
+        cout << "\n--- MENU ---\n";
+        cout << "1. Agregar\n";
+        cout << "2. Mostrar\n";
+        cout << "3. Eliminar\n";
+        cout << "4. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch(opcion) {
+            case 1:
+                cout << "Opcion Agregar seleccionada\n";
+                break;
+            case 2:
+                cout << "Opcion Mostrar seleccionada\n";
+                break;
+            case 3:
+                cout << "Opcion Eliminar seleccionada\n";
+                break;
+            case 4:
+                cout << "Saliendo...\n";
+                break;
+            default:
+                cout << "Opcion invalida\n";
+        }
+
+    } while (opcion != 4);
+
+    return 0;
+}
